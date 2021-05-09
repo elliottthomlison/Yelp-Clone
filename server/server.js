ffb0c9ce72ee1714d09796a271a3c0b222801e17
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-09 03:12:30 -0700

    Adjust the routes and prepare to establish them further

--- server/server.js
+++ server/server.js
@@ -4,7 +4,12 @@ const express = require("express")
 //create an instance of express = achieved by storing it into a variable
 const app = express()
 
-app.get('')
+app.get('/getRestaurants', (req, res) => {
+  res.json({
+    status: 'success',
+    restaurant: "mcdonalds",
+  }) 
+})
 
 //via .env the port number is called and process.env.PORT calls that number
 //use || as a backup in case the first port number is being used
