68bef73d1bfbda1ac1aa97d35088eb7538588728
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-09 01:36:19 -0700

    Install nodemon and adjust the port functionality in server.js

--- server/server.js
+++ server/server.js
@@ -4,8 +4,11 @@ const express = require("express")
 //create an instance of express = achieved by storing it into a variable
 const app = express()
 
+app.get('')
+
 //via .env the port number is called and process.env.PORT calls that number
-const port = process.env.PORT
-app.listen(8008, () => {
+//use || as a backup in case the first port number is being used
+const port = process.env.PORT || 8008
+app.listen(port, () => {
   console.log(`Server is up and listening on ${port}`);
 });
