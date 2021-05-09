d3b4dd75eda1cc5c31abf385bc6594e221b87fd5
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-09 01:00:13 -0700

    Create and install .env file and use in server.js's port number

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const express = require("express")
 
 //create an instance of express = achieved by storing it into a variable
 const app = express()
 
-const port = 8008
+//via .env the port number is called and process.env.PORT calls that number
+const port = process.env.PORT
 app.listen(8008, () => {
   console.log(`Server is up and listening on ${port}`);
 });
