2d17c3c3b5f6bca58fa190b455b0544fb12081e1
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-09 10:56:52 -0700

    Create more routes for delete, update, etc. in the server.js file

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
-const express = require("express")
-
+const express = require('express')
+const morgan = require('morgan')
 //create an instance of express = achieved by storing it into a variable
 const app = express()
+
+app.use(express.json)
 
 //get grabs multiple restaurants
 app.get('/api/v1/restaurants', (req, res) => {
@@ -13,16 +15,33 @@ app.get('/api/v1/restaurants', (req, res) => {
     }
   }); 
 });
-
+ 
 //get a single restaurant
 app.get('/api/v1/restaurants/:id', (req, res) => {
   console.log(req.params)
+
+  res.status(200).json({
+    status: 'success',
+  })
 })
 
 //create restaurant
 app.post('/api/v1/restaurants', (req, res) => {
-  console.log(req);
+  console.log(req.body);
 })
+
+//update restaurants
+//by putting 'id' it allows express know what restaurant we want to update 
+app.put('/api/v1/restaurants/:id', (req, res) => {
+  console.log(req.params.id)
+})
+
+//delete restaurant
+app.delete('/api/v1/restaurants/:id', (req, res) => {
+  res.status(204).json({
+    status: 'success'
+  });
+});
 
 //via .env the port number is called and process.env.PORT calls that number
 //use || as a backup in case the first port number is being used
