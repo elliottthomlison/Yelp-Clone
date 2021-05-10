11dcae4b255532dfea9afd7532e815546dd7b909
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-10 08:36:50 -0700

    Complete the bootstrap form under the header

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -10,8 +10,16 @@ const AddRestaurant = () => {
             </div>
             <div className="col">
               <input className="form-control" type="text" placeholder="location"/>
-              <select className="custom-select my-1 mr-sm-2"></select>
+              <select className="custom-select my-1 mr-sm-2">
+                <option value="disabled">Price Range</option>
+                <option value="1">$</option>
+                <option value="2">$$</option>
+                <option value="3">$$$</option>
+                <option value="4">$$$$</option>
+                <option value="5">$$$$$</option>
+              </select>
             </div>
+            <button className="btn btn-primary">Add</button>
           </div>
       </form>
     </div>
