6e2ba10dece2d12a7c43d627ada42ecfa26c82aa
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-09 17:36:13 -0700

    Setup the routes for the project and create the files related to the pages themselves

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 const App = () => {
-    return <div>App</div>
+    return <div>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route exact path='/restaurants/:id/update' component={UpdatePage}/>
+          <Route exact path='/restaurants/:id' component={RestaurantDetailPage}/>
+        </Switch>
+      </Router>
+    </div>
 }
 
 export default App;;
